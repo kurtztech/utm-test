a32d31cab9c1c4920d1cbe7b19b3758013bf8cfb
Sam Kurtz <sam@krtz.tech>
2023-12-24 12:24:24 -0700

    move to env

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ import Script from "next/script";
 export default function Home() {
   return (
     <div className="container">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-M3H1QQQ20G" />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+      />
       <Script id="google-analytics">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-M3H1QQQ20G');
+        gtag('config', ${process.env.GA_MEASUREMENT_ID}});
       `}
       </Script>
 
